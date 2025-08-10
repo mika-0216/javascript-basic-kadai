e0e0a2ad56cfde34da7b829dcbf0cf1b78c17f10
mika-0216 <mika.20010216@icloud.com>
2025-08-10 21:42:51 +0900

    kadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date("2025-08-10");
+ const year = today.getFullYear(); 
+const month = today.getMonth() + 1; 
+const day = today.getDate(); 
+
+console.log(year + '年' + month + '月' + day + '日');
