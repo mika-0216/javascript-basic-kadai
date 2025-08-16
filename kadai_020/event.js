b6ec4b164ff038f74890e9a60c33caf1df294cb6
mika-0216 <mika.20010216@icloud.com>
2025-08-17 08:46:10 +0900

    kadai_020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    const text = document.getElementById('text')
+
+    text.textContent = 'ボタンをクリックしました';
+    
+    
+});
+
+text.style.fontFamily = 'Meiryo, Arial, sans-serif';
